7f4bedd6cfa65d5ace106cdc30e32b2eb518224e
Clayton Roberto Dos Santos <clayton.silveira87@hotmail.com>
2026-01-23 14:10:21 -0300

    estudos-em-Dart

---
+++ aula_dart/aula1_lista.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> frutas = ['Maça', 'Laranja', 'Banana'];
+
+  print(frutas[0]);
+
+  frutas.add('Goiaba');
+
+  print(frutas);
+
+  //frutas.remove('Maça');
+
+  print(frutas);
+}
